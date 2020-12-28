dd493eba14bf839d8d33f2676a5403d670262214
Ronaldo Barreto <ronaldo.cardoso@gmail.com>
2020-12-27 21:48:25 -0300

    Update index.js
    Change for Google Fulfillment Parameters

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function getMqttClient() {
 var mqttClient = getMqttClient();
 
 app.set('port', settings.http_port);
-app.use(bodyParser.json());
+app.use(bodyParser.json({ parameters: '*/*' }));
 
 function logRequest(req, res, next) {
     var ip = req.headers['x-forwarded-for'] ||
